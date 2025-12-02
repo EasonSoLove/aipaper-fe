88142ae67ed60b2467c2410122a2bd59465b4501
gaoqiao <669218881@qq.com>
2025-12-02 19:18:39 +0800

    触发部署

--- src/views/404.vue
+++ src/views/404.vue
@@ -37,7 +37,7 @@
         <div class="bullshit__headline">{{ message }}</div>
         <div class="bullshit__info">
           Please check that the URL you entered is correct, or click the button
-          below to return to the homepage.
+          below to return to the homePage
         </div>
         <a href="" class="bullshit__return-home">Back to home.</a>
       </div>
